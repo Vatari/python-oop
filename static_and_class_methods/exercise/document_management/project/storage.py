de9a6d78c54bd7ab66c8f6f232b0d1b6a5fb9906
vatari <majestic13@gmail.com>
2023-11-30 08:27:00 +0200

    exercise 1

---
+++ static_and_class_methods/exercise/document_management/project/storage.py
@@ -0,0 +1,53 @@
+class Storage:
+    def __init__(self):
+        self.categories = []
+        self.topics = []
+        self.documents = []
+
+    def add_category(self, category):
+        if category not in self.categories:
+            self.categories.append(category)
+
+    def add_topic(self, topic):
+        if topic not in self.topics:
+            self.topics.append(topic)
+
+    def add_document(self, document):
+        if document not in self.documents:
+            self.documents.append(document)
+
+    def edit_category(self, category_id, new_name):
+        for category in self.categories:
+            if category.id == category_id:
+                category.name = new_name
+
+    def edit_topic(self, topic_id, new_topic, new_storage_folder):
+        for topic in self.topics:
+            if topic.id == topic_id:
+                topic.topic = new_topic
+                topic.storage_folder = new_storage_folder
+
+    def edit_document(self, document_id, new_file_name):
+        for document in self.documents:
+            if document.id == document_id:
+                document.edit(new_file_name)
+
+    def delete_category(self, category_id):
+        self.categories = [category for category in self.categories if category.id != category_id]
+
+    def delete_topic(self, topic_id):
+        self.topics = [topic for topic in self.topics if topic.id != topic_id]
+
+    def delete_document(self, document_id):
+        self.documents = [document for document in self.documents if document.id != document_id]
+
+    def get_document(self, document_id):
+        for document in self.documents:
+            if document.id == document_id:
+                return document
+
+    def __repr__(self):
+        result = ""
+        for document in self.documents:
+            result += repr(document) + "\n"
+        return result
